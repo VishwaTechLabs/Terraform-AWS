5fb2a7925033291a46b392f57a28e2cd910f7501
iampamidlmvishwa <iam.infosecvishwa@gmail.com>
2026-05-31 00:26:10 +0530

    Create provder.tf

---
+++ without_vars_tfvars/provder.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+
+    }
+
+  }
+
+}
+
+provider "aws" {
+
+  region = "ap-south-1"
+
+  access_key = "YOUR_ACCESS_KEY"
+
+  secret_key = "YOUR_SECRET_KEY"
+
+}
